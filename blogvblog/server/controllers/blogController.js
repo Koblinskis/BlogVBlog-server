431295f30e23fe22a7b4ba22f3ed1a7828ffae01
Micheal Koblinski <stevenkoblinskip@gmail.com>
2020-01-15 18:15:36 -0500

    fix winner route

--- blogvblog/server/controllers/blogController.js
+++ blogvblog/server/controllers/blogController.js
@@ -3,7 +3,6 @@ const Blogs = require('../model/blogs')
 exports.blogs_versus_get = async function(req, res) {
   try {
     const blogs = await Blogs.getBlogTitles()
-    console.log(blogs)
     res.send(blogs)
 
   } catch (e) {
@@ -42,22 +41,22 @@ exports.blogs_winner_post = async function(req, res) {
   try{
     const winnerId = req.body.winner
     const loserId = req.body.loser
-    const winnerBlog = await Blogs.findById(winnerId)
-    const loserBlog = await Blogs.findById(loserId)
-    const idObj = { id: loserId, count: 1}
-    const versusArr = winnerBlog.versus.concat(idObj)
-    const update = { $inc: { score: 1 }, versus: versusArr }
+
+    const updateWinner = { $inc: { score: 1, 
+      [`versus.${loserId}`]: 1
+    } }
+    const updateLoser = { $inc: { score: -1, 
+      [`versus.${winnerId}`]: -1
+    } }
     
-    const newBlog = await Blogs.findByIdAndUpdate(winnerId, update, { new: true })
-    if(!newBlog) {
+    const winnerResults = await Blogs.findByIdAndUpdate(winnerId, updateWinner, { new: true, upsert: true })
+    const loserResults = await Blogs.findByIdAndUpdate(loserId, updateLoser, { new: true, upsert: true })
+    if(!winnerResults || !loserResults) {
       return res.status(404).send('No Blog Exists')
     }
-    res.send([newBlog, winnerBlog, loserBlog])
+    res.send([winnerResults, loserResults])
   
   } catch (e) {
     res.status(500).send(e)
   }
 }
-
-//5e1a1b3a35791dd5ad712187
-//5e1a1b3a35791dd5ad7121e4
--- blogvblog/server/model/blogs.js
+++ blogvblog/server/model/blogs.js
@@ -16,7 +16,7 @@ const blogSchema = new mongoose.Schema({
     type: Number,
   },
   versus: {
-    type: Array,
+    type: Object,
   }
 })
 
