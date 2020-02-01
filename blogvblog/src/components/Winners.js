99458fc32fb6d19c04861dbcba51dcd224312f75
Micheal Koblinski <stevenkoblinskip@gmail.com>
2020-02-01 14:25:18 -0500

    added a chart to the winners page

--- blogvblog/src/components/Winners.js
+++ blogvblog/src/components/Winners.js
@@ -1,4 +1,9 @@
 import React from 'react'
+import MyChart from './MyChart'
+
+import { Chart } from 'chart.js'
+
+
 
 class Winners extends React.Component {
   state = {
@@ -6,11 +11,91 @@ class Winners extends React.Component {
     winners: []
   }
 
+  chartRef = React.createRef();
+
   componentDidMount = async () => {
     const winnersAsync = await this.getWinners()
     console.log(winnersAsync)
     winnersAsync.forEach(winner => {
       this.setState((preState) => ({ winners: preState.winners.concat(winner)}))
+    });
+    const myChartRef = this.chartRef.current.getContext("2d");
+        
+    new Chart(myChartRef, {
+      type: "bar",
+      data: {
+        //Bring in data
+        winners: this.state.winners.map(winner => {
+          let str = winner.title
+          let title = str.charAt(0).toUpperCase() + str.slice(1);
+          return title
+        }),
+        labels: this.state.winners.map(winner => {
+          let str = winner._id
+          let title = str.charAt(0).toUpperCase() + str.slice(1);
+          return title
+        }),
+        datasets: [{
+          label: "Score",
+          data: this.state.winners.map(winner => winner.score),
+            //backgroundColor:'green',
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+            ],
+            borderWidth:1,
+            borderColor:'#777',
+            hoverBorderWidth:3,
+            hoverBorderColor:'#000'
+        }],
+        
+      },
+      backgroundColor:[
+        'rgba(255, 99, 132, 0.6)',
+        'rgba(54, 162, 235, 0.6)',
+        'rgba(255, 206, 86, 0.6)',
+        'rgba(75, 192, 192, 0.6)',
+        'rgba(153, 102, 255, 0.6)',
+        'rgba(255, 159, 64, 0.6)',
+        'rgba(255, 99, 132, 0.6)'
+      ],
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              callback: function(value) {
+                if (value.length > 10) {
+                  return value.substr(0, 10) + '...'; //truncate
+                } else {
+                  return value
+                }
+              },
+            }
+          }]
+        },
+        tooltips: {
+          callbacks: {
+            title: function(tooltipItems, data) {
+              var idx = tooltipItems[0].index;
+              var title = data.winners[idx]
+              return title; //do something with title
+            },
+          }
+      }
+      }
     });
   }
 
@@ -33,8 +118,12 @@ class Winners extends React.Component {
       <div className="winner__big-box">
         <h1 className="winner__title">Winners</h1>
         <div className="winner__box">
+        <canvas
+                    id="myChart"
+                    ref={this.chartRef}
+                />
           {this.state.winners.map(winner => (<div key={winner.id}><p className="winner__category">{winner._id}:</p>   <p className="winner__winning-title">{winner.title}<hr/>{winner.score}</p></div>))}
-
+          
         </div>
       </div>
     )
