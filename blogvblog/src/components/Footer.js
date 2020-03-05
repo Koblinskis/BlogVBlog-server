b62eb91b5eca29092a8a167a3b985373b53315b5
Michael Koblinski <stevenkoblinskiw@gmail.com>
2020-03-05 11:03:05 -0500

    added link to github

--- blogvblog/src/components/Footer.js
+++ blogvblog/src/components/Footer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const Footer = () => (
-  <div className="footer__box">
-    <div className="footer__box-left">© S. Michael Koblinski</div>
-    <div className="footer__box-right">Made with ❤️</div>
-  </div>
+  <a href="https://github.com/Koblinskis/BlogVBlog">
+    <div className="footer__box">
+      <div className="footer__box-left">© S. Michael Koblinski</div>
+      <div className="footer__box-right">Made with ❤️</div>
+    </div>
+  </a>
 )
 
 export default Footer
