b1eb05049508cb1ef12f401bb2045b3ae52137e3
Michael Koblinski <stevenkoblinskiw@gmail.com>
2020-03-05 11:04:04 -0500

    removed spacing

--- blogvblog/src/components/Home.js
+++ blogvblog/src/components/Home.js
@@ -5,9 +5,9 @@ const Home = () => (
   <div>
     <h1 className="home__title">Blog V Blog</h1>
     <div className="home__text">
-      <p>This webpage allows people to vote on titles of blogs they find most interesting.</p><br/>
-      <p>Vote for the title that seems the most appeasing.</p><br/>
-      <p className="home__small-text">Created by Michael Koblinski</p>
+      <p>This webpage allows people to vote on titles of blogs they find most interesting.</p>
+      <p>Vote for the title that seems the most appeasing.</p>
+      <p className="home__small-text">Created by S. Michael Koblinski</p>
       <p className="home__small-text">Uses MongoDB for a server. <br/>React for the frontend, and <br/>NodeJS for the backend</p>
     </div>
     <button className="home__button">
