cd6fc29ce6fd1d1d4b9754fa0ccce97ed5af68fa
Michael Koblinski <stevenkoblinskiw@gmail.com>
2020-03-05 11:03:44 -0500

    hided unused link

--- blogvblog/src/components/Header.js
+++ blogvblog/src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => (
     <NavLink to="/" className="header__title">Blog V Blog</NavLink>
     <NavLink to="/versus" activeClassName="header__active-link" className="header__text">Versus</NavLink>
     <NavLink to="/winners" activeClassName="header__active-link" className="header__text">Winners</NavLink>
-    <NavLink to="/hire" activeClassName="header__active-link" className="header__text">Hire Me!</NavLink>
+    {/*<NavLink to="/hire" activeClassName="header__active-link" className="header__text">Hire Me!</NavLink>*/}
   </div>
 )
 
